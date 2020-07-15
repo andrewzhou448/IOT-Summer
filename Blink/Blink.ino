c38a21ccee820816e2449c132c1a0fa92c52d4af
andrewzhou448 <68338351+andrewzhou448@users.noreply.github.com>
2020-07-15 13:50:55 -0400

    Create a blink file

---
+++ Blink/Blink.ino
@@ -0,0 +1,30 @@
+/*
+  ESP8266 Blink by Simon Peter
+  Blink the blue LED on the ESP-01 module
+  This example code is in the public domain
+
+  The blue LED on the ESP-01 module is connected to GPIO1
+  (which is also the TXD pin; so we cannot use Serial.print() at the same time)
+
+  Note that this sketch uses LED_BUILTIN to find the pin with the internal LED
+*/
+
+
+int LED_PIN = 16;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  pinMode(LED_PIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, LOW);  // Turn the LED off by making the voltage HIGH
+  digitalWrite(LED_PIN, HIGH);   // Turn the LED on (Note that LOW is the voltage level
+  delay(500);   
+  // but actually the LED is on; this is because
+  // it is active low on the ESP-01)
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LED_PIN, LOW);  // Turn the LED off by making the voltage HIGH                // Wait for a second  // Turn the LED off by making the voltage HIGH
+  delay(500);                      // Wait for two seconds (to demonstrate the active low LED)
+}
